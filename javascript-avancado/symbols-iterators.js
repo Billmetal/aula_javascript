ffc8d9b5f21493c2e9f0b947cdbc6530885087a9
Bill <billmetal9@hotmail.com>
2020-07-22 14:41:01 -0300

    Exemplos da aula de Generators .

--- javascript-avancado/symbols-iterators.js
+++ javascript-avancado/symbols-iterators.js
@@ -36,3 +36,44 @@ const it = obj[Symbol.iterator]();
 
 console.log(it.next());
 
+// Generators
+
+function* hello(){
+    console.log("Hello");
+    yield 1;
+
+    console.log("From");
+    const value = yield 2;
+
+    console.log("Function !");
+    console.log(value);
+}
+
+
+function* naturalNumbers(){
+    let number = 0;
+    while(true){
+        yield number;
+        number++;
+    }
+}
+
+const it = naturalNumbers();
+
+console.log(it.next());
+console.log(it.next());
+console.log(it.next());
+
+const obj = {
+    values: [1,2,3,4],
+    *[Symbol.iterator]() {
+        for(var i = 0; i < this.values.length; i++){
+            yield this.values[i];
+        }
+    }
+};
+
+for(let value of obj){
+    console.log(value);
+}
+
