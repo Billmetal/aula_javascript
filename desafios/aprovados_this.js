db003ca3ebdc47705bc426f3488ee478df30c32d
Billmetal <billmetal9@gmail.com>
2022-06-15 20:53:23 -0300

    Desafios Aprovados e Calcula Idade.

---
+++ desafios/aprovados_this.js
@@ -0,0 +1,65 @@
+// atividade 1
+
+const alunos = [{
+    nome: "João",
+    nota: 5,
+    turma: "1B"
+},
+{
+    nome: "Sofia",
+    nota: 9,
+    turma: "1B"
+},
+{
+    nome: "Paulo",
+    nota: 6,
+    turma: "2C"
+},
+{
+    nome: "Miguel",
+    nota: 3,
+    turma: "2C"
+}];
+
+
+function alunosAprovados(arr, media) {
+    let aprovados = [];
+    for (let i = 0; i < arr.length; i++) {
+        const { nota, nome } = arr[i];
+        if (arr[i].nota >= media) { 
+            aprovados.push(arr[i].nome);
+        }
+    }
+    return aprovados;
+}
+ 
+console.log(alunosAprovados(alunos, 6));
+
+
+// Atividade 2
+
+function calculaIdade(anos) { 
+    return `Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
+}
+
+const pessoa1 = {
+    nome: "Maria",
+    idade: 30
+};
+
+const pessoa2 = {
+    nome: "Carla",
+    idade: 26
+};
+
+const animal = {
+    nome: "Fiona",
+    idade: 5,
+    raca: "Pug"
+};
+
+console.log(calculaIdade.call(pessoa2, 30));
+console.log(calculaIdade.call(animal, 7));
+
+console.log(calculaIdade.apply(pessoa1, [4]));
+console.log(calculaIdade.apply(animal,[4]));
